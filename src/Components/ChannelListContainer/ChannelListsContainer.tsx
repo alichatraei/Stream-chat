9f8f19c4bacbd6751983605e3d2622bedb126020
Ali chatraei <m.saeri@fanap.ir>
2021-09-09 18:28:09 +0430

    Import two components in channel lists container

--- src/Components/ChannelListContainer/ChannelListsContainer.tsx
+++ src/Components/ChannelListContainer/ChannelListsContainer.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ChannelList, useChatContext } from 'stream-chat-react'
 import Cookies from 'universal-cookie'
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from '../'
+
 import SchoolIcon from '../../Assets/icons/SchoolIcon.png'
 import LogoutIcon from '../../Assets/icons/LogoutIcon.png'
 const SideBar = () => (
@@ -30,6 +31,18 @@ const ChannelListsContainer = () => {
             <div className="channel-lists__list__wrapper">
                 <SchoolHeader />
                 <ChannelSearch />
+                <ChannelList filters={{}}
+                    List={(listProps) => (
+                        <TeamChannelList {...listProps} type="team" error={false} />
+                    )}
+                    Preview={(previewProps) => <TeamChannelPreview {...previewProps}
+                        type="team" />} />
+                <ChannelList filters={{}}
+                    List={(listProps) => (
+                        <TeamChannelList {...listProps} type="messaging" error={false} />
+                    )}
+                    Preview={(previewProps) => <TeamChannelPreview {...previewProps}
+                        type="messaging" />} />
             </div>
         </>
     )
